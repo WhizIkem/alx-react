072e44cd6b321b0d572c3281d7e6ff21e1907e7b
whizikem <wisdomikem@gmail.com>
2023-09-14 01:40:35 +0100

    Chained mutations

---
+++ 0x06-React_Immutable/4-mutations.js
@@ -0,0 +1,17 @@
+import { Map } from 'immutable';
+
+const map = Map({
+  1: 'Liam',
+  2: 'Noah',
+  3: 'Elijah',
+  4: 'Oliver',
+  5: 'Jacob',
+  6: 'Lucas',
+});
+
+const map2 = map.withMutations((mutableMap) => {
+  mutableMap.set(2, 'Benjamin');
+  mutableMap.set(4, 'Oliver');
+});
+
+export { map, map2 };
