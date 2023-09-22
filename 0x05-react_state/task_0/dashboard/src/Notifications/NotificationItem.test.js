bf5803caeb25dcc4f69b836675a9fe9db9e97894
whizikem <wisdomikem@gmail.com>
2023-09-22 21:25:01 +0100

    Adding a local state for notifications

--- 0x05-react_state/task_0/dashboard/src/Notifications/NotificationItem.test.js
+++ 0x05-react_state/task_0/dashboard/src/Notifications/NotificationItem.test.js
@@ -13,34 +13,33 @@ afterEach(() => {
 describe("rendering components", () => {
   it("renders NotificationItem component without crashing", () => {
     const wrapper = shallow(<NotificationItem />);
-
     expect(wrapper.exists()).toBe(true);
   });
 
   it('renders correct html from type="default" value="test" props', () => {
-    const wrapper = shallow(<NotificationItem />);
-
-    wrapper.setProps({ type: "default", value: "test" });
-    expect(wrapper.html()).toEqual('<li class="default_1tsdo2i" data-notification-type="default">test</li>');
+    const wrapper = shallow(<NotificationItem type="default" value="test" />);
+    expect(wrapper.find('li[data-notification-type="default"]').text()).toBe(
+      "test"
+    );
   });
 
   it('renders correct html from  html="<u>test</u>" props', () => {
-    const wrapper = shallow(<NotificationItem />);
-
-    wrapper.setProps({ html: "<u>test</u>" });
-    expect(wrapper.html()).toEqual('<li data-urgent="true" class="urgent_137u7ef"><u>test</u></li>');
+    const wrapper = shallow(
+    <NotificationItem html="<u>test</u>" />
+  );
+  expect(wrapper.find('li[data-urgent="true"] > u').html()).toEqual(
+    '<u>test</u></li>'
+    );
   });
 });
 
 describe("onclick event behaves as it should", () => {
-  it("should call console.log", () => {
-    const wrapper = shallow(<NotificationItem />);
+  it("should call MarkAsRead with the correct id when clicked", () => {
     const spy = jest.fn();
-
-    wrapper.setProps({ value: "test item", markAsRead: spy, id: 1 });
-    wrapper.find("li").props().onClick();
-    expect(spy).toBeCalledTimes(1);
-    expect(spy).toBeCalledWith(1);
-    spy.mockRestore();
+    const wrapper = shallow(
+      <NotificationItem value="test item" markAsRead={spy} id={1} />
+    );
+    wrapper.find('li[data-notification-type="default"]').simulate("click");
+    expect(spy).toHaveBeenCalledWith(1);
   });
 });
