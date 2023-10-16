62933c4207589d943f2e4edd7c4bdf102c1ebeab
whizikem <wisdomikem@gmail.com>
2023-10-16 16:26:02 +0100

    first commit

---
+++ 0x09-react_redux_connectors_and_providers/task_1/dashboard/src/App/App.js
@@ -0,0 +1,124 @@
+import React from "react";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+import Login from "../Login/Login";
+import CourseList from "../CourseList/CourseList";
+import Notifications from "../Notifications/Notifications";
+import BodySectionWithMarginBottom from "../BodySection/BodySectionWithMarginBottom";
+import BodySection from "../BodySection/BodySection";
+import { StyleSheet, css } from "aphrodite";
+import PropTypes from "prop-types";
+import { getLatestNotification } from "../utils/utils";
+import { AppContext, user } from "./AppContext";
+import { connect } from "react-redux";
+import {
+  displayNotificationDrawer,
+  hideNotificationDrawer
+} from "../actions/uiActionCreators"; // Make sure the import path is correct
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleKeyPress = this.handleKeyPress.bind(this);
+    this.logIn = this.logIn.bind(this);
+  }
+
+  listCourses = [
+    { id: 1, name: "ES6", credit: 60 },
+    { id: 2, name: "Webpack", credit: 20 },
+    { id: 3, name: "React", credit: 40 },
+  ];
+
+  listNotifications = [
+    { id: 1, type: "default", value: "New course available" },
+    { id: 2, type: "urgent", value: "New resume available" },
+    { id: 3, type: "urgent", html: getLatestNotification() },
+  ];
+
+  handleKeyPress(e) {
+    if (e.ctrlKey && e.key === "h") {
+      e.preventDefault();
+      alert("Logging you out");
+      this.props.hideNotificationDrawer();
+    }
+  }
+
+  componentDidMount() {
+    if (typeof document !== "undefined") {
+      document.addEventListener("keydown", this.handleKeyPress);
+    }
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener("keydown", this.handleKeyPress);
+  }
+
+  logIn(email, password) {
+    // Your login logic here
+  }
+
+  render() {
+    return (
+      <AppContext.Provider value={{ user: this.props.user }}>
+        <div className={css(styles.App)}>
+          <div className="heading-section">
+            <button className="displayDrawer" onClick={this.props.displayNotificationDrawer}>Open</button>
+            <Notifications
+              listNotifications={this.listNotifications}
+              displayDrawer={this.props.displayDrawer}
+              handleDisplayDrawer={this.props.displayNotificationDrawer}
+              handleHideDrawer={this.props.hideNotificationDrawer}
+            />
+            <Header />
+          </div>
+          {this.props.isLoggedIn ? (
+            <BodySectionWithMarginBottom title="Course list">
+              <CourseList listCourses={this.listCourses} />
+            </BodySectionWithMarginBottom>
+          ) : (
+            <BodySectionWithMarginBottom title="Log in to continue">
+              <Login logIn={this.logIn} />
+            </BodySectionWithMarginBottom>
+          )}
+          <BodySection title="News from the school">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis at tempora odio, necessitatibus repudiandae reiciendis cum nemo sed asperiores ut molestiae eaque aliquam illo ipsa iste vero dolor voluptates.
+            </p>
+          </BodySection>
+          <Footer />
+        </div>
+      </AppContext.Provider>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  App: {
+    height: "100vh",
+    maxWidth: "100vw",
+    position: "relative",
+    fontFamily: "Arial, Helvetica, sans-serif",
+  },
+});
+
+App.propTypes = {
+  user: PropTypes.object.isRequired,
+  isLoggedIn: PropTypes.bool,
+  displayDrawer: PropTypes.bool,
+  displayNotificationDrawer: PropTypes.func,
+  hideNotificationDrawer: PropTypes.func
+};
+
+const mapStateToProps = (state) => ({
+  user: state.user,
+  isLoggedIn: state.isLoggedIn,
+  displayDrawer: state.isNotificationDrawerVisible
+});
+
+const mapDispatchToProps = {
+  displayNotificationDrawer,
+  hideNotificationDrawer
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
